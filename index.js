fd97af5af7c51c95c5ec5449a40aa90775ea234d
John Jakobsen <49962408+jerikjakobsen@users.noreply.github.com>
2023-09-28 15:38:36 -0400

    log params

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function upload(params) {
   return new Promise((resolve) => {
     s3.upload(params, (err, data) => {
       if (err) core.error(err);
-      core.info(`uploaded params Key - ${params.Key}`);
+      core.info(`uploaded params Key - ${params}`);
       core.info(`uploaded - ${data.Key}`);
       core.info(`located - ${data.Location}`);
       resolve(data.Location);
